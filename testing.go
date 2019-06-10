7fece539b3b647112ddee3ec5959f93c296695fe
Fernando Mayo <fernando@undefinedlabs.com>
2019-06-10 22:07:43 +0200

    Autodetect test suite and test status

--- testing.go
+++ testing.go
@@ -2,11 +2,39 @@ package scopeagent
 
 import (
 	"github.com/opentracing/opentracing-go"
+	"runtime"
+	"strings"
 	"testing"
 )
 
 func InstrumentTest(t *testing.T, f func(t *testing.T)) {
-	span := opentracing.StartSpan(t.Name(), opentracing.Tags{"span.kind": "test"})
+	pc, _, _, _ := runtime.Caller(1)
+	parts := strings.Split(runtime.FuncForPC(pc).Name(), ".")
+	pl := len(parts)
+	packageName := ""
+	funcName := parts[pl-1]
+
+	if parts[pl-2][0] == '(' {
+		funcName = parts[pl-2] + "." + funcName
+		packageName = strings.Join(parts[0:pl-2], ".")
+	} else {
+		packageName = strings.Join(parts[0:pl-1], ".")
+	}
+
+	span := opentracing.StartSpan(t.Name(), opentracing.Tags{
+		"span.kind":  "test",
+		"test.name":  funcName,
+		"test.suite": packageName,
+	})
 	defer span.Finish()
+	defer func() {
+		if t.Failed() {
+			span.SetTag("test.status", "FAIL")
+		} else if t.Skipped() {
+			span.SetTag("test.status", "SKIP")
+		} else {
+			span.SetTag("test.status", "PASS")
+		}
+	}()
 	f(t)
 }
