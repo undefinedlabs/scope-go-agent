dee2fec3c0418c0e844503c89d42d8e477775c6b
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2019-08-30 12:21:56 +0200

    Adds support for test.code tag

---
+++ ast/sources.go
@@ -0,0 +1,86 @@
+package ast
+
+import (
+	"go/ast"
+	"go/parser"
+	"go/token"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+var (
+	methodCodes 	map[string]map[string]*MethodCode
+	mutex			sync.Mutex
+)
+
+type MethodCode struct {
+	Package string
+	Name 	string
+	File	string
+	Start	CodePos
+	End		CodePos
+}
+type CodePos struct {
+	Line 	int
+	Column 	int
+}
+
+// Gets the function source code boundaries from the caller method
+func GetFuncSourceFromCaller(skip int) *MethodCode {
+	pc, _, _, _ := runtime.Caller(skip + 1)
+	return GetFuncSource(pc)
+}
+
+// Gets the function source code boundaries from a method
+func GetFuncSource(pc uintptr) *MethodCode {
+	mFunc := runtime.FuncForPC(pc)
+	mFile, _ := mFunc.FileLine(pc)
+
+	mutex.Lock()
+	if methodCodes == nil {
+		methodCodes = map[string]map[string]*MethodCode{}
+	}
+	if methodCodes[mFile] == nil {
+		methodCodes[mFile] = map[string]*MethodCode{}
+
+		fSet := token.NewFileSet()
+		f, err := parser.ParseFile(fSet, mFile, nil, 0)
+		if err != nil {
+			return nil
+		}
+
+		packageName := f.Name.String()
+		for _, decl := range f.Decls {
+			if fDecl, ok := decl.(*ast.FuncDecl); ok {
+				bPos := fDecl.Pos()
+				if fDecl.Body != nil {
+					bEnd := fDecl.Body.End()
+					if bPos.IsValid() && bEnd.IsValid() {
+						pos := fSet.PositionFor(bPos, true)
+						end := fSet.PositionFor(bEnd, true)
+						methodCode := MethodCode{
+							Package: packageName,
+							Name: fDecl.Name.String(),
+							File: mFile,
+							Start: CodePos{
+								Line:   pos.Line,
+								Column: pos.Column,
+							},
+							End: CodePos{
+								Line:   end.Line,
+								Column: end.Column,
+							},
+						}
+						methodCodes[mFile][methodCode.Name] = &methodCode
+					}
+				}
+			}
+		}
+	}
+	mutex.Unlock()
+
+	funcName := mFunc.Name()
+	funcNameParts := strings.Split(funcName, ".")
+	return methodCodes[mFile][funcNameParts[1]]
+}
--- testing.go
+++ testing.go
@@ -2,7 +2,9 @@ package scopeagent
 
 import (
 	"context"
+	"fmt"
 	"github.com/opentracing/opentracing-go"
+	"github.com/undefinedlabs/go-agent/ast"
 	"github.com/undefinedlabs/go-agent/errors"
 	"runtime"
 	"strings"
@@ -35,10 +37,17 @@ func StartTest(t *testing.T) *Test {
 		packageName = strings.Join(parts[0:pl-1], ".")
 	}
 
+	sourceBounds := ast.GetFuncSource(pc)
+	var testCode string
+	if sourceBounds != nil {
+		testCode = fmt.Sprintf("%s:%d:%d", sourceBounds.File, sourceBounds.Start.Line, sourceBounds.End.Line)
+	}
+
 	span, ctx := opentracing.StartSpanFromContext(context.Background(), t.Name(), opentracing.Tags{
 		"span.kind":  "test",
 		"test.name":  funcName,
 		"test.suite": packageName,
+		"test.code": testCode,
 	})
 	span.SetBaggageItem("trace.kind", "test")
 
