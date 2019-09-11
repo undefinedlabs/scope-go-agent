9a241a4cb840de3bc91be925def25d7851aae174
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2019-09-11 10:22:13 +0200

    Update envCarrier.go

--- envCarrier.go
+++ envCarrier.go
@@ -3,6 +3,7 @@ package scopeagent
 import (
 	"fmt"
 	"github.com/opentracing/opentracing-go"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -13,7 +14,7 @@ type envCarrier struct {
 func (carrier *envCarrier) Set(key, val string) {
 	var newCarrier []string
 	keyUpper := strings.ToUpper(key)
-	ctxKey := "CTX_" + keyUpper
+	ctxKey := escape("CTX_" + keyUpper)
 	if carrier.Env != nil {
 		for _, item := range *carrier.Env {
 			if strings.Index(item, ctxKey) < 0 {
@@ -29,7 +30,7 @@ func (carrier *envCarrier) ForeachKey(handler func(key, val string) error) error
 		for _, item := range *carrier.Env {
 			if strings.Index(item, "CTX_") >= 0 {
 				kv := strings.Split(item, "=")
-				err := handler(kv[0][4:], kv[1])
+				err := handler(unescape(kv[0][4:]), kv[1])
 				if err != nil {
 					return err
 				}
@@ -39,6 +40,18 @@ func (carrier *envCarrier) ForeachKey(handler func(key, val string) error) error
 	return nil
 }
 
+// We need to sanitize the env vars due:
+// Environment variable names used by the utilities in the Shell and Utilities volume of IEEE Std 1003.1-2001
+// consist solely of uppercase letters, digits, and the '_' (underscore)
+func escape(value string) string {
+	return value
+}
+func unescape(value string) string {
+	return value
+}
+
+
+// Injects the test context to the command environment variables
 func (test *Test) Inject(command *exec.Cmd) *exec.Cmd {
 	var carrier opentracing.TextMapWriter
 	carrier = &envCarrier{}
@@ -49,8 +62,15 @@ func (test *Test) Inject(command *exec.Cmd) *exec.Cmd {
 	command.Env = append(command.Env, *carrier.(*envCarrier).Env...)
 	return command
 }
-func (test *Test) extract(env []string) (opentracing.SpanContext, error) {
+
+// Extract the context from an environment variables array
+func extract(env []string) (opentracing.SpanContext, error) {
 	var carrier opentracing.TextMapReader
 	carrier = &envCarrier{Env:&env}
 	return GlobalAgent.Tracer.Extract(opentracing.TextMap, carrier)
+}
+
+// Gets the current span context from the environment variables
+func getContextFromEnvironment() (opentracing.SpanContext, error) {
+	return extract(os.Environ())
 }
