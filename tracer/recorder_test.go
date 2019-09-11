16640c7b175d45bdfc24e348f3c2bc2ade66202d
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2019-09-11 11:59:10 +0200

    Adds NTP support (#42)
    * Adds NTP support

--- tracer/recorder_test.go
+++ tracer/recorder_test.go
@@ -1,11 +1,10 @@
 package tracer
 
 import (
+	"github.com/stretchr/testify/assert"
+	"go.undefinedlabs.com/scopeagent/ntp"
 	"sync/atomic"
 	"testing"
-	"time"
-
-	"github.com/stretchr/testify/assert"
 )
 
 func TestInMemoryRecorderSpans(t *testing.T) {
@@ -14,7 +13,7 @@ func TestInMemoryRecorderSpans(t *testing.T) {
 	span := RawSpan{
 		Context:   SpanContext{},
 		Operation: "test-span",
-		Start:     time.Now(),
+		Start:     ntp.Now(),
 		Duration:  -1,
 	}
 	apiRecorder.RecordSpan(span)
