1baada412d429e6ffb78117edf48ea30aafd5ae0
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2019-09-11 18:28:04 +0200

    Update agent.go

--- agent.go
+++ agent.go
@@ -32,8 +32,9 @@ var (
 
 	version = "0.1.0-dev"
 
-	gitDataOnce sync.Once
-	gitData     *GitData
+	printReportOnce sync.Once
+	gitDataOnce     sync.Once
+	gitData         *GitData
 )
 
 func init() {
@@ -153,20 +154,22 @@ func (a *Agent) Stop() {
 }
 
 func (a *Agent) printReport() {
-	if a.testingMode && a.recorder.totalSend > 0 {
-		if a.recorder.koSend == 0 {
-			fmt.Printf("\n** Scope Test Report **\n\n")
-			fmt.Println("Access the detailed test report for this build at:")
-			fmt.Printf("   %s/external/v1/results/%s\n\n", a.scopeEndpoint, a.agentId)
-		} else if a.recorder.koSend < a.recorder.totalSend {
-			fmt.Printf("\n** Scope Test Report **\n\n")
-			fmt.Println("There was a problem sending data to Scope, partial test report for this build at:")
-			fmt.Printf("   %s/external/v1/results/%s\n\n", a.scopeEndpoint, a.agentId)
-		} else {
-			_, _ = fmt.Fprintf(os.Stderr, "\n** Scope Test Report **\n\n")
-			_, _ = fmt.Fprintf(os.Stderr, "There was a problem sending data to Scope\n")
+	printReportOnce.Do(func() {
+		if a.testingMode && a.recorder.totalSend > 0 {
+			if a.recorder.koSend == 0 {
+				fmt.Printf("\n** Scope Test Report **\n\n")
+				fmt.Println("Access the detailed test report for this build at:")
+				fmt.Printf("   %s/external/v1/results/%s\n\n", a.scopeEndpoint, a.agentId)
+			} else if a.recorder.koSend < a.recorder.totalSend {
+				fmt.Printf("\n** Scope Test Report **\n\n")
+				fmt.Println("There was a problem sending data to Scope, partial test report for this build at:")
+				fmt.Printf("   %s/external/v1/results/%s\n\n", a.scopeEndpoint, a.agentId)
+			} else {
+				_, _ = fmt.Fprintf(os.Stderr, "\n** Scope Test Report **\n\n")
+				_, _ = fmt.Fprintf(os.Stderr, "There was a problem sending data to Scope\n")
+			}
 		}
-	}
+	})
 }
 
 func (a *Agent) Flush() error {
