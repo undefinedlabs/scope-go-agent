b8692254ded6dc975c4c13b60d0d3ca525ab5346
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2019-09-12 12:33:08 +0200

    Adds the Container detection algorithm (#56)

---
+++ container.go
@@ -0,0 +1,40 @@
+package scopeagent
+
+import (
+	"bufio"
+	"os"
+	"runtime"
+	"strings"
+	"sync"
+)
+
+var (
+	runningInContainerOnce sync.Once
+	runningInContainer     bool
+)
+
+// gets if the current process is running inside a container
+func isRunningInContainer() bool {
+	runningInContainerOnce.Do(func() {
+		if runtime.GOOS == "linux" {
+			file, err := os.Open("/proc/1/cgroup")
+			if err != nil {
+				runningInContainer = false
+				return
+			}
+			defer file.Close()
+			for {
+				line, readErr := bufio.NewReader(file).ReadString('\n')
+				if readErr != nil {
+					break
+				}
+				if strings.Contains(line, "/docker/") || strings.Contains(line, "/lxc/") {
+					runningInContainer = true
+					return
+				}
+			}
+		}
+		runningInContainer = false
+	})
+	return runningInContainer
+}
