1bc9e68945e9112f1c8a5a56b99b22d00902c175
Fernando Mayo <fernando@undefinedlabs.com>
2019-10-11 15:58:00 +0200

    Remove autoinstall, add more agent options (#71)
    * Remove autoinstall, add more agent options
    
    * Send empty arrays instead of null in recorder

--- init.go
+++ init.go
@@ -1,58 +1,33 @@
 package scopeagent // import "go.undefinedlabs.com/scopeagent"
 
 import (
-	"github.com/opentracing/opentracing-go"
 	"go.undefinedlabs.com/scopeagent/agent"
-	"go.undefinedlabs.com/scopeagent/instrumentation"
 	scopetesting "go.undefinedlabs.com/scopeagent/instrumentation/testing"
 	"runtime"
 	"testing"
 )
 
-var globalAgent *agent.Agent
-
-// Tries to automatically install the Scope agent if we can autodetect the API key, otherwise does nothing
-func init() {
-	defaultAgent, err := agent.NewAgent()
-	if err != nil {
-		return
-	}
-
-	globalAgent = defaultAgent
-	instrumentation.SetTracer(globalAgent.Tracer())
-
-	if agent.GetBoolEnv("SCOPE_SET_GLOBAL_TRACER", false) {
-		opentracing.SetGlobalTracer(globalAgent.Tracer())
-	}
-}
-
-// Returns the autoinstalled agent instance, if any
-func GlobalAgent() *agent.Agent {
-	return globalAgent
-}
+var defaultAgent *agent.Agent
 
 // Helper function to run a `testing.M` object and gracefully stopping the agent afterwards
-func Run(m *testing.M) int {
-	if globalAgent != nil {
-		globalAgent.SetTestingMode(true)
-		defer globalAgent.Stop()
+func Run(m *testing.M, opts ...agent.Option) int {
+	opts = append(opts, agent.WithTestingModeEnabled())
+	newAgent, err := agent.NewAgent(opts...)
+	if err != nil {
+		return m.Run()
 	}
-	return m.Run()
-}
 
-// Gracefully stops the Scope agent, flushing any buffers before returning
-func Stop() {
-	if globalAgent != nil {
-		globalAgent.Stop()
-	}
+	defer newAgent.Stop()
+	defaultAgent = newAgent
+	return m.Run()
 }
 
 // Instruments the given test, returning a `Test` object that can be used to extend the test trace
 func StartTest(t *testing.T, opts ...scopetesting.Option) *scopetesting.Test {
 	opts = append(opts, scopetesting.WithOnPanicHandler(func(test *scopetesting.Test) {
-		if globalAgent != nil {
-			_ = globalAgent.Flush()
-			globalAgent.PrintReport()
+		if defaultAgent != nil {
+			_ = defaultAgent.Flush()
+			defaultAgent.PrintReport()
 		}
 	}))
 	pc, _, _, _ := runtime.Caller(1)
