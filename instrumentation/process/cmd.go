2bf9dcc168aa454c2a6409ee5818073d112f9680
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2019-10-29 08:30:53 +0100

    Cmd OperationName (#82)
    * operation name builder
    * Update cmd.go
    
    * Support for cmd arguments in operation name
    
    * Update instrumentation/process/cmd.go
    
    Co-Authored-By: Fernando Mayo <fernando@undefinedlabs.com>
    
    * Update instrumentation/process/cmd.go
    
    Co-Authored-By: Fernando Mayo <fernando@undefinedlabs.com>
    
    * Update instrumentation/process/cmd.go
    
    Co-Authored-By: Fernando Mayo <fernando@undefinedlabs.com>
    
    * Update instrumentation/process/cmd.go
    
    Co-Authored-By: Fernando Mayo <fernando@undefinedlabs.com>
    
    * Update instrumentation/process/cmd.go
    
    Co-Authored-By: Fernando Mayo <fernando@undefinedlabs.com>
    
    * StarSpanWithOperationName as private and ignoring .idea folder
    
    * Update .gitignore
    
    * Delete .gitignore
    
    * Removes the IDE folder

--- instrumentation/process/cmd.go
+++ instrumentation/process/cmd.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"github.com/opentracing/opentracing-go"
 
@@ -24,10 +25,31 @@ func InjectToCmd(ctx context.Context, command *exec.Cmd) *exec.Cmd {
 
 // Injects a new span context to the command environment variables
 func InjectToCmdWithSpan(ctx context.Context, command *exec.Cmd) (opentracing.Span, context.Context) {
-	innerSpan, innerCtx := opentracing.StartSpanFromContextWithTracer(ctx, instrumentation.Tracer(), "Exec: "+filepath.Base(command.Args[0]))
+	innerSpan, innerCtx := opentracing.StartSpanFromContextWithTracer(ctx, instrumentation.Tracer(),
+		"Exec: "+getOperationNameFromArgs(command.Args))
 	innerSpan.SetTag("Args", command.Args)
 	innerSpan.SetTag("Path", command.Path)
 	innerSpan.SetTag("Dir", command.Dir)
 	InjectToCmd(innerCtx, command)
 	return innerSpan, innerCtx
+}
+
+func getOperationNameFromArgs(args []string) string {
+	if args == nil || len(args) == 0 {
+		return ""
+	}
+	var operationNameBuilder = new(strings.Builder)
+	operationNameBuilder.WriteString(filepath.Base(args[0]))
+	operationNameBuilder.WriteRune(' ')
+	for _, item := range args[1:] {
+		if strings.ContainsRune(item, ' ') {
+			operationNameBuilder.WriteRune('"')
+			operationNameBuilder.WriteString(item)
+			operationNameBuilder.WriteRune('"')
+		} else {
+			operationNameBuilder.WriteString(item)
+		}
+		operationNameBuilder.WriteRune(' ')
+	}
+	return operationNameBuilder.String()
 }
