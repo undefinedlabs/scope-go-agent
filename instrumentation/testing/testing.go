a2195f1e159032cf32cf2d14d66c361a13a07292
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2019-10-30 17:46:45 +0100

    Update instrumentation/testing/testing.go
    Co-Authored-By: Fernando Mayo <fernando@undefinedlabs.com>

--- instrumentation/testing/testing.go
+++ instrumentation/testing/testing.go
@@ -241,7 +241,7 @@ func StartBenchmark(b *testing.B, pc uintptr, benchFunc func(b *testing.B)) {
 	avg := math.Round((float64(results.T.Nanoseconds())/float64(results.N))*100) / 100
 	span.SetTag("benchmark.runs", results.N)
 	span.SetTag("benchmark.duration.mean", avg)
-	span.SetTag("benchmark.memory.avg_allocations", results.AllocsPerOp())
+	span.SetTag("benchmark.memory.mean_allocations", results.AllocsPerOp())
 	span.SetTag("benchmark.memory.avg_bytes_allocations", results.AllocedBytesPerOp())
 	if result {
 		span.SetTag("test.status", "PASS")
