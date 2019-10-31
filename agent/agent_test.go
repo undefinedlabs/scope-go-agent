5e5433f2e8c2cd12c34fe2d58cc2eb088980bda6
Daniel Redondo <tonyredondo@gmail.com>
2019-10-31 08:20:40 +0100

    go fmt

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 func TestDsnParser(t *testing.T) {
-	 dsnValues := [][]string {
-		 { "https://4432432432432423@shared.scope.dev", "4432432432432423", "https://shared.scope.dev" },
-		 { "http://4432432432432423@shared.scope.dev", "4432432432432423", "http://shared.scope.dev" },
-		 { "https://4432432432432423:ignored@shared.scope.dev", "4432432432432423", "https://shared.scope.dev" },
-		 { "https://4432432432432423:ignored@shared.scope.dev/custom/path", "4432432432432423", "https://shared.scope.dev/custom/path" },
-		 { "https://4432432432432423:ignored@scope.dev", "4432432432432423", "https://scope.dev" },
+	dsnValues := [][]string{
+		{"https://4432432432432423@shared.scope.dev", "4432432432432423", "https://shared.scope.dev"},
+		{"http://4432432432432423@shared.scope.dev", "4432432432432423", "http://shared.scope.dev"},
+		{"https://4432432432432423:ignored@shared.scope.dev", "4432432432432423", "https://shared.scope.dev"},
+		{"https://4432432432432423:ignored@shared.scope.dev/custom/path", "4432432432432423", "https://shared.scope.dev/custom/path"},
+		{"https://4432432432432423:ignored@scope.dev", "4432432432432423", "https://scope.dev"},
 
-		 { "4432432432432423@shared.scope.dev", "", "4432432432432423@shared.scope.dev" },
-		 { "noise", "", "noise" },
-	 }
+		{"4432432432432423@shared.scope.dev", "", "4432432432432423@shared.scope.dev"},
+		{"noise", "", "noise"},
+	}
 
 	for i := 0; i < len(dsnValues); i++ {
 		dsnValue := dsnValues[i]
