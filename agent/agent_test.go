8a9a7ce49ab0c9d43a4e89761e45a83bf774e303
Daniel Redondo <tonyredondo@gmail.com>
2019-11-01 23:53:00 +0100

    Update agent_test.go

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -14,7 +14,7 @@ func TestMain(m *testing.M) {
 func TestDsnParser(t *testing.T) {
 	runner.TestStart(t)
 	defer runner.TestEnd(t)
-	
+
 	dsnValues := [][]string{
 		{"https://4432432432432423@shared.scope.dev", "4432432432432423", "https://shared.scope.dev"},
 		{"http://4432432432432423@shared.scope.dev", "4432432432432423", "http://shared.scope.dev"},
@@ -43,9 +43,11 @@ func TestDsnParser(t *testing.T) {
 }
 
 func TestSkipped(t *testing.T) {
-
+	runner.TestStart(t)
+	defer runner.TestEnd(t)
 }
 
 func TestFirstTest(t *testing.T) {
-
+	runner.TestStart(t)
+	defer runner.TestEnd(t)
 }
