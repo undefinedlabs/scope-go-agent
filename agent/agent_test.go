cd7da8896396c215f371eaf0781472287306d12c
Daniel Redondo <tonyredondo@gmail.com>
2019-11-03 17:57:36 +0100

    Update agent_test.go

--- agent/agent_test.go
+++ agent/agent_test.go
@@ -2,15 +2,8 @@ package agent
 
 import (
 	"fmt"
-	"go.undefinedlabs.com/scopeagent/runner"
-	"math/rand"
-	"os"
 	"testing"
 )
-
-func TestMain(m *testing.M) {
-	os.Exit(runner.Run(m, "repo", "br", "cmmt", "default"))
-}
 
 func TestDsnParser(t *testing.T) {
 	dsnValues := [][]string{
@@ -38,29 +31,4 @@ func TestDsnParser(t *testing.T) {
 			}
 		})
 	}
-}
-
-func TestSkipped(t *testing.T) {
-}
-
-func TestFirstTest(t *testing.T) {
-}
-
-func TestFail(t *testing.T) {
-	t.FailNow()
-}
-
-func TestFlaky(t *testing.T) {
-	value := rand.Intn(8)
-	t.Log("Value", value)
-	if value <= 5 {
-		t.FailNow()
-	}
-}
-
-func TestError(t *testing.T) {
-	a := 0
-	b := 5 / a
-	_ = b
-	t.FailNow()
 }
