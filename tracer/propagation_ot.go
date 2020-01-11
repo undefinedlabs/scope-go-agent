689d561b261b680ec21719e521f049db580b0ae7
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-01-11 02:45:05 +0100

    Extract priority

--- tracer/propagation_ot.go
+++ tracer/propagation_ot.go
@@ -90,23 +90,9 @@ func (p *textMapPropagator) Extract(
 	var sampled bool
 	var err error
 	decodedBaggage := make(map[string]string)
+
 	err = carrier.ForeachKey(func(k, v string) error {
 		switch strings.ToLower(k) {
-		case fieldNameTraceID:
-			traceID, err = uuid.Parse(v)
-			if err != nil {
-				return opentracing.ErrSpanContextCorrupted
-			}
-		case fieldNameSpanID:
-			spanID, err = strconv.ParseUint(v, 16, 64)
-			if err != nil {
-				return opentracing.ErrSpanContextCorrupted
-			}
-		case fieldNameSampled:
-			sampled, err = strconv.ParseBool(v)
-			if err != nil {
-				return opentracing.ErrSpanContextCorrupted
-			}
 		case traceParentKey:
 			if len(v) < 55 {
 				return opentracing.ErrSpanContextCorrupted
@@ -137,10 +123,6 @@ func (p *textMapPropagator) Extract(
 				}
 			}
 		default:
-			lowercaseK := strings.ToLower(k)
-			if strings.HasPrefix(lowercaseK, prefixBaggage) {
-				decodedBaggage[strings.TrimPrefix(lowercaseK, prefixBaggage)] = v
-			}
 			// Balance off the requiredFieldCount++ just below...
 			requiredFieldCount--
 		}
@@ -150,6 +132,40 @@ func (p *textMapPropagator) Extract(
 	if err != nil {
 		return nil, err
 	}
+	if traceID == uuid.Nil && spanID == 0 {
+		err = carrier.ForeachKey(func(k, v string) error {
+			switch strings.ToLower(k) {
+			case fieldNameTraceID:
+				traceID, err = uuid.Parse(v)
+				if err != nil {
+					return opentracing.ErrSpanContextCorrupted
+				}
+			case fieldNameSpanID:
+				spanID, err = strconv.ParseUint(v, 16, 64)
+				if err != nil {
+					return opentracing.ErrSpanContextCorrupted
+				}
+			case fieldNameSampled:
+				sampled, err = strconv.ParseBool(v)
+				if err != nil {
+					return opentracing.ErrSpanContextCorrupted
+				}
+			default:
+				lowercaseK := strings.ToLower(k)
+				if strings.HasPrefix(lowercaseK, prefixBaggage) {
+					decodedBaggage[strings.TrimPrefix(lowercaseK, prefixBaggage)] = v
+				}
+				// Balance off the requiredFieldCount++ just below...
+				requiredFieldCount--
+			}
+			requiredFieldCount++
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
+	}
+
 	if requiredFieldCount < tracerStateFieldCount {
 		if requiredFieldCount == 0 {
 			return nil, opentracing.ErrSpanContextNotFound
