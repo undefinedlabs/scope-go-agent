9c63e54d4e8f573a0ff82773b9d3460dc6a6054e
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-01-14 14:54:14 +0100

    Test based on the Regex

--- instrumentation/testing/testing_test.go
+++ instrumentation/testing/testing_test.go
@@ -1,12 +1,11 @@
 package testing
 
 import (
-	"github.com/opentracing/opentracing-go"
-	"github.com/opentracing/opentracing-go/log"
+	"strings"
 	"testing"
 )
 
-func TestExtractTestLogBuffer(t *testing.T) {
+func TestLogBufferRegex(t *testing.T) {
 	test := StartTest(t)
 	defer test.End()
 
@@ -16,52 +15,20 @@ func TestExtractTestLogBuffer(t *testing.T) {
 		"Hello\n World\nMulti\n        Line",
 	}
 
-	tester := &spanTester{}
-	test.span = tester
-
 	for _, item := range logLines {
 		t.Log(item)
 	}
 
-	test.extractTestLoggerOutput()
-
-	for i := 0; i < len(logLines); i++ {
-		logItem := tester.logFields[i]
-
-		if logItem[3].Value() != logLines[i] {
+	logBuffer := extractTestOutput(t)
+	logs := string(*logBuffer)
+	for idx, matches := range TESTING_LOG_REGEX.FindAllStringSubmatch(logs, -1) {
+		message := strings.ReplaceAll(matches[3], "\n        ", "\n")
+		if logLines[idx] != message {
 			t.FailNow()
 		}
 	}
 }
 
-type spanTester struct {
-	logFields [][]log.Field
+func TestExtractSubTestLogBuffer(t *testing.T) {
+	t.Run("SubTest", TestLogBufferRegex)
 }
-
-func (s *spanTester) Finish()                                          {}
-func (s *spanTester) FinishWithOptions(opts opentracing.FinishOptions) {}
-func (s *spanTester) Context() opentracing.SpanContext {
-	return nil
-}
-func (s *spanTester) SetOperationName(operationName string) opentracing.Span {
-	return nil
-}
-func (s *spanTester) SetTag(key string, value interface{}) opentracing.Span {
-	return nil
-}
-func (s *spanTester) LogFields(fields ...log.Field) {
-	s.logFields = append(s.logFields, fields)
-}
-func (s *spanTester) LogKV(alternatingKeyValues ...interface{}) {}
-func (s *spanTester) SetBaggageItem(restrictedKey, value string) opentracing.Span {
-	return nil
-}
-func (s *spanTester) BaggageItem(restrictedKey string) string {
-	return ""
-}
-func (s *spanTester) Tracer() opentracing.Tracer {
-	return nil
-}
-func (s *spanTester) LogEvent(event string)                                 {}
-func (s *spanTester) LogEventWithPayload(event string, payload interface{}) {}
-func (s *spanTester) Log(data opentracing.LogData)                          {}
