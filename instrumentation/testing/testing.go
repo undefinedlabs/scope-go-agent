40f3234fb7ae7fae1342bf9754f014a9168be540
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-01-17 15:15:56 +0100

    comments changes

--- instrumentation/testing/testing.go
+++ instrumentation/testing/testing.go
@@ -163,7 +163,7 @@ func (test *Test) Context() context.Context {
 	return test.ctx
 }
 
-// Runs a sub test
+// Runs an auto instrumented sub test
 func (test *Test) Run(name string, f func(t *testing.T)) {
 	pc, _, _, _ := runtime.Caller(1)
 	test.t.Run(name, func(childT *testing.T) {
