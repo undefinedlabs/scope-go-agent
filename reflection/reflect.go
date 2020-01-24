b982b3fcad755dcdaa39d6a30bedffdf9e8bb904
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-01-24 16:13:58 +0100

    Refactor

--- reflection/reflect.go
+++ reflection/reflect.go
@@ -28,3 +28,14 @@ func GetFieldPointerOfT(t *testing.T, fieldName string) (unsafe.Pointer, error) 
 	}
 	return nil, errors.New("field can't be retrieved")
 }
+
+// Gets a pointer of a private or public field in a testing.B struct
+func GetFieldPointerOfB(b *testing.B, fieldName string) (unsafe.Pointer, error) {
+	val := reflect.Indirect(reflect.ValueOf(b))
+	member := val.FieldByName(fieldName)
+	if member.IsValid() {
+		ptrToY := unsafe.Pointer(member.UnsafeAddr())
+		return ptrToY, nil
+	}
+	return nil, errors.New("result can't be retrieved")
+}
