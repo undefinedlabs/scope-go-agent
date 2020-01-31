4eece5f8b11babdcbf560ecfb90be3e8b5150cae
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-01-31 10:26:38 +0100

    comment

--- instrumentation/logging/logger_go11.go
+++ instrumentation/logging/logger_go11.go
@@ -18,7 +18,7 @@ func getStdLoggerWriter() io.Writer {
 
 // Gets the writer of a custom logger
 func getLoggerWriter(logger *stdlog.Logger) io.Writer {
-	// There is not API on Go1.11 to get the current writer, accessing by reflection.
+	// There is not API in Go1.11 to get the current writer, accessing by reflection.
 	if ptr, err := GetFieldPointerOfLogger(logger, "out"); err == nil {
 		return *(*io.Writer)(ptr)
 	}
