1e35f0846b5e056744e44367a2fda012d73b15e0
Daniel Redondo <tonyredondo@gmail.com>
2020-02-05 01:13:50 +0100

    - Added support for Env Vars configuration - Changed exitOnError to panicAsFail - Runner changes to avoid the `[exec]` subtest to appear in the standard output (by setting the chatty and name field of a test)

--- agent/util.go
+++ agent/util.go
@@ -18,6 +18,18 @@ func getBoolEnv(key string, fallback bool) bool {
 	return value
 }
 
+func getIntEnv(key string, fallback int) int {
+	stringValue, exists := os.LookupEnv(key)
+	if !exists {
+		return fallback
+	}
+	value, err := strconv.ParseInt(stringValue, 0, 0)
+	if err != nil {
+		panic(fmt.Sprintf("unable to parse %s - should be 'true' or 'false'", key))
+	}
+	return int(value)
+}
+
 func addToMapIfEmpty(dest map[string]interface{}, source map[string]interface{}) {
 	for k, newValue := range source {
 		if oldValue, ok := dest[k]; !ok || oldValue == "" {
