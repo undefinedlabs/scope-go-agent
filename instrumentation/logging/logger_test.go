4e85da88e657337854ac87033835671690757d4e
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-02-06 13:25:17 +0100

    Instrumentation tests (#128)
    * GRPC test
    
    * Logger tests and stdout patch fix.
    
    * Http Tests
    
    * fix
    
    * http server test
    
    * Process instrumentation test
    
    * Runner test

--- instrumentation/logging/logger_test.go
+++ instrumentation/logging/logger_test.go
@@ -3,8 +3,14 @@ package logging
 import (
 	"fmt"
 	stdlog "log"
+	"os"
 	"regexp"
 	"testing"
+	"time"
+
+	"github.com/opentracing/opentracing-go"
+
+	"go.undefinedlabs.com/scopeagent/tags"
 )
 
 func TestLoggingRegex(t *testing.T) {
@@ -57,4 +63,72 @@ func TestLoggingRegex(t *testing.T) {
 			}
 		})
 	}
+}
+
+func TestStandardLoggerInstrumentation(t *testing.T) {
+	stdlog.SetFlags(stdlog.LstdFlags | stdlog.Lmicroseconds | stdlog.Llongfile)
+	PatchStandardLogger()
+	defer UnpatchStandardLogger()
+
+	messages := []string{"Print log", "Println log"}
+
+	stdlog.Print(messages[0])
+	stdlog.Println(messages[1])
+
+	checkRecords(t, messages)
+}
+
+func TestCustomLoggerInstrumentation(t *testing.T) {
+	logger := stdlog.New(os.Stdout, "", stdlog.LstdFlags|stdlog.Lmicroseconds|stdlog.Llongfile)
+	PatchLogger(logger)
+	defer UnpatchLogger(logger)
+
+	messages := []string{"Print log", "Println log"}
+
+	logger.Print(messages[0])
+	logger.Println(messages[1])
+
+	checkRecords(t, messages)
+}
+
+func TestStdOutInstrumentation(t *testing.T) {
+	PatchStdOut()
+	defer UnpatchStdOut()
+	Reset()
+
+	messages := []string{"Println log", "Println log"}
+
+	fmt.Println(messages[0])
+	fmt.Println(messages[1])
+
+	<-time.After(time.Second)
+
+	checkRecords(t, messages)
+}
+
+func checkRecords(t *testing.T, messages []string) {
+	records := GetRecords()
+
+	if len(records) != 2 {
+		t.Fatalf("error in the number of records, expected: 2, actual: %d", len(records))
+	}
+
+	for _, msg := range messages {
+		if !checkMessage(msg, records) {
+			t.Fatalf("the message '%s' was not in the log records", msg)
+		}
+	}
+}
+
+func checkMessage(msg string, records []opentracing.LogRecord) bool {
+	for _, rec := range records {
+		for _, field := range rec.Fields {
+			if field.Key() == tags.EventMessage {
+				if msg == fmt.Sprint(field.Value()) {
+					return true
+				}
+			}
+		}
+	}
+	return false
 }
---
+++ instrumentation/nethttp/nethttp_test.go
@@ -0,0 +1,128 @@
+package nethttp
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/opentracing/opentracing-go"
+
+	"go.undefinedlabs.com/scopeagent/instrumentation"
+	"go.undefinedlabs.com/scopeagent/tracer"
+)
+
+var r *tracer.InMemorySpanRecorder
+
+func TestMain(m *testing.M) {
+	PatchHttpDefaultClient()
+
+	// Test tracer
+	r = tracer.NewInMemoryRecorder()
+	instrumentation.SetTracer(tracer.New(r))
+
+	os.Exit(m.Run())
+}
+
+func TestHttpClient(t *testing.T) {
+	r.Reset()
+	_, ctx := opentracing.StartSpanFromContextWithTracer(context.Background(), instrumentation.Tracer(), "Test")
+
+	req, err := http.NewRequest("GET", "https://www.google.com/", nil)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	req = req.WithContext(ctx)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	if resp.StatusCode != 200 {
+		t.Fatalf("server returned %d status code", resp.StatusCode)
+	}
+	resp.Body.Close()
+
+	spans := r.GetSpans()
+	if len(spans) != 1 {
+		t.Fatalf("there aren't the right number of spans: %d", len(spans))
+	}
+	checkTags(t, spans[0].Tags, map[string]string{
+		"component":     "net/http",
+		"http.method":   "GET",
+		"http.url":      "https://www.google.com/",
+		"peer.hostname": "www.google.com",
+		"peer.port":     "443",
+	})
+}
+
+func TestHttpServer(t *testing.T) {
+	r.Reset()
+	sp, ctx := opentracing.StartSpanFromContextWithTracer(context.Background(), instrumentation.Tracer(), "Test")
+	sp.SetBaggageItem("trace.kind", "test")
+
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		_, err := fmt.Fprintf(w, "Hello world")
+		if err != nil {
+			w.WriteHeader(500)
+			return
+		}
+	})
+	server := httptest.NewServer(Middleware(nil))
+
+	url := fmt.Sprintf("%s/hello", server.URL)
+	req, err := http.NewRequest("GET", url, nil)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	req = req.WithContext(ctx)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		t.Fatalf("%+v", err)
+	}
+	if resp.StatusCode != 200 {
+		t.Fatalf("server returned %d status code", resp.StatusCode)
+	}
+	resp.Body.Close()
+	server.Close()
+
+	spans := r.GetSpans()
+	if len(spans) != 2 {
+		t.Fatalf("there aren't the right number of spans: %d", len(spans))
+	}
+	checkTags(t, spans[0].Tags, map[string]string{
+		"component":        "net/http",
+		"http.method":      "GET",
+		"http.url":         "/hello",
+		"span.kind":        "server",
+		"http.status_code": "200",
+	})
+	checkTags(t, spans[1].Tags, map[string]string{
+		"component":   "net/http",
+		"http.method": "GET",
+		"http.url":    url,
+		"peer.ipv4":   "127.0.0.1",
+		"span.kind":   "client",
+	})
+}
+
+func checkTags(t *testing.T, tags map[string]interface{}, expected map[string]string) {
+	for eK, eV := range expected {
+		if ok, aV := checkTag(tags, eK, eV); !ok {
+			if aV == "" {
+				t.Fatalf("the tag with key = '%s' was not found in the span tags", eK)
+			} else {
+				t.Fatalf("the tag with key = '%s' has a different value in the span tags. Expected = '%s', Actual = '%s'", eK, eV, aV)
+			}
+		}
+	}
+}
+
+func checkTag(tags map[string]interface{}, key string, expectedValue string) (bool, string) {
+	if val, ok := tags[key]; ok {
+		sVal := fmt.Sprint(val)
+		return expectedValue == sVal, sVal
+	}
+	return false, ""
+}
