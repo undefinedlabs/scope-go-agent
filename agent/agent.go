14ab9e76fc770e34b1d892615c51a0aeb8df2e62
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-02-10 12:12:52 +0100

    Scope log path fix (#151)
    * temporal path fix
    
    * temporal path fix
    
    * temporal path fix
    
    * changes based on the review

--- agent/agent.go
+++ agent/agent.go
@@ -399,22 +399,19 @@ func getLogPath() (string, error) {
 	}
 
 	if logFolder != "" {
-		isOk := true
-		// If folder doesn't exist we try to create it
-		if _, err := os.Stat(logFolder); os.IsNotExist(err) {
-			mkErr := os.Mkdir(logFolder, os.ModeDir)
-			if mkErr != nil {
-				isOk = false
-			}
-		}
-		if isOk {
+		if _, err := os.Stat(logFolder); err == nil {
+			return logFolder, nil
+		} else if os.IsNotExist(err) && os.Mkdir(logFolder, os.ModeDir) == nil {
 			return logFolder, nil
 		}
 	}
 
 	// If the log folder can't be used we return a temporal path, so we don't miss the agent logs
-	if dir, err := ioutil.TempDir("", "scope"); err != nil {
-		return dir, nil
+	logFolder = path.Join(os.TempDir(), "scope")
+	if _, err := os.Stat(logFolder); err == nil {
+		return logFolder, nil
+	} else if os.IsNotExist(err) && os.Mkdir(logFolder, os.ModeDir) == nil {
+		return logFolder, nil
 	} else {
 		return "", err
 	}
