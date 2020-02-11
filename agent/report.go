892b5c36005f7ebd59a8ba56872a05259d9b1d88
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-02-11 16:43:22 +0100

    Apply suggestions from code review
    Co-Authored-By: Fernando Mayo <fernando@undefinedlabs.com>

--- agent/report.go
+++ agent/report.go
@@ -7,9 +7,9 @@ import (
 
 func (a *Agent) PrintReport() {
 	printReportOnce.Do(func() {
-		if a.testingMode && (a.recorder.stats.totalSpans > 0 || a.recorder.stats.spansRejected > 0) {
+		if a.testingMode && a.recorder.stats.totalTestSpans > 0 {
 			fmt.Printf("\n** Scope Test Report **\n")
-			if a.recorder.stats.spansSent == a.recorder.stats.totalSpans && a.recorder.stats.spansRejected == 0 {
+			if a.recorder.stats.testSpansNotSent == 0 {
 				fmt.Println("Access the detailed test report for this build at:")
 				fmt.Printf("   %s\n\n", a.getUrl(fmt.Sprintf("external/v1/results/%s", a.agentId)))
 			} else {
@@ -18,7 +18,7 @@ func (a *Agent) PrintReport() {
 				}
 				a.recorder.writeStats()
 				fmt.Println("There was a problem sending data to Scope.")
-				if a.recorder.stats.spansSent > 0 {
+				if a.recorder.stats.testSpansSent > 0 {
 					fmt.Println("Partial results for this build are available at:")
 					fmt.Printf("   %s\n\n", a.getUrl(fmt.Sprintf("external/v1/results/%s", a.agentId)))
 				}
