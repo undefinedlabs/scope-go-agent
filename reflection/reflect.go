a6745662f0bc71710f2f469f13c7d73f1a339b7e
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-02-18 12:40:04 +0100

    restore

--- reflection/reflect.go
+++ reflection/reflect.go
@@ -10,22 +10,22 @@ import (
 
 // Gets a pointer of a private or public field in a testing.M struct
 func GetFieldPointerOfM(m *testing.M, fieldName string) (unsafe.Pointer, error) {
-	return GetFieldPointerOf(m, fieldName)
+	return getFieldPointerOf(m, fieldName)
 }
 
 // Gets a pointer of a private or public field in a testing.T struct
 func GetFieldPointerOfT(t *testing.T, fieldName string) (unsafe.Pointer, error) {
-	return GetFieldPointerOf(t, fieldName)
+	return getFieldPointerOf(t, fieldName)
 }
 
 // Gets a pointer of a private or public field in a testing.B struct
 func GetFieldPointerOfB(b *testing.B, fieldName string) (unsafe.Pointer, error) {
-	return GetFieldPointerOf(b, fieldName)
+	return getFieldPointerOf(b, fieldName)
 }
 
 // Gets a pointer of a private or public field in a testing.T struct
 func GetFieldPointerOfLogger(logger *stdlog.Logger, fieldName string) (unsafe.Pointer, error) {
-	return GetFieldPointerOf(logger, fieldName)
+	return getFieldPointerOf(logger, fieldName)
 }
 
 // Gets the type pointer to a field name
@@ -37,7 +37,7 @@ func GetTypePointer(i interface{}, fieldName string) (reflect.Type, error) {
 	return nil, errors.New("field can't be retrieved")
 }
 
-func GetFieldPointerOf(i interface{}, fieldName string) (unsafe.Pointer, error) {
+func getFieldPointerOf(i interface{}, fieldName string) (unsafe.Pointer, error) {
 	val := reflect.Indirect(reflect.ValueOf(i))
 	member := val.FieldByName(fieldName)
 	if member.IsValid() {
