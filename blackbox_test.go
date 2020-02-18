f4a3a3ed070a324bba25519ede654b9d019b3141
Daniel Redondo <daniel.redondo@undefinedlabs.com>
2020-02-18 01:04:44 +0100

    black box test file

---
+++ blackbox_test.go
@@ -0,0 +1,15 @@
+/*
+This file contains complete black box tests of the agent, avoiding any cycle reference
+*/
+
+package scopeagent_test
+
+import (
+	"testing"
+
+	_ "go.undefinedlabs.com/scopeagent/autoinstrument"
+)
+
+func TestComplete(t *testing.T) {
+	// TODO
+}
