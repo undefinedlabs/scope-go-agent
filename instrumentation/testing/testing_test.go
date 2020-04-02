214ddd595a3dc9e6320f8ade89f1248c47ed324a
Daniel Redondo <tonyredondo@gmail.com>
2020-04-02 13:29:15 +0200

    Test instrumentation init benchmark

--- instrumentation/testing/testing_test.go
+++ instrumentation/testing/testing_test.go
@@ -30,3 +30,23 @@ func TestLogBufferRegex(t *testing.T) {
 func TestExtractSubTestLogBuffer(t *testing.T) {
 	t.Run("SubTest", TestLogBufferRegex)
 }
+
+func BenchmarkTestInit(b *testing.B) {
+	for i:=0; i < b.N; i++ {
+		tests := append(make([]testing.InternalTest, 0),
+			testing.InternalTest{ Name: "Test01", F: func(t *testing.T) {} },
+			testing.InternalTest{ Name: "Test02", F: func(t *testing.T) {} },
+			testing.InternalTest{ Name: "Test03", F: func(t *testing.T) {} },
+			testing.InternalTest{ Name: "Test04", F: func(t *testing.T) {} },
+			testing.InternalTest{ Name: "Test05", F: func(t *testing.T) {} },
+		)
+		benchmarks := append(make([]testing.InternalBenchmark, 0),
+			testing.InternalBenchmark{ Name: "Test01", F: func(b *testing.B) {} },
+			testing.InternalBenchmark{ Name: "Test02", F: func(b *testing.B) {} },
+			testing.InternalBenchmark{ Name: "Test03", F: func(b *testing.B) {} },
+			testing.InternalBenchmark{ Name: "Test04", F: func(b *testing.B) {} },
+			testing.InternalBenchmark{ Name: "Test05", F: func(b *testing.B) {} },
+		)
+		Init(testing.MainStart(nil, tests, benchmarks, nil))
+	}
+}
