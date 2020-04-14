0d63302b300a758436f787d8866deb9f9f208b7d
drodriguezhdez <daniel.rguez.hdez.github@gmail.com>
2020-04-14 11:44:34 +0200

    Use random pool to generate opentracing IDs

--- tracer/util.go
+++ tracer/util.go
@@ -1,25 +1,29 @@
 package tracer
 
 import (
-	"math/rand"
-	"sync"
+	"runtime"
 	"time"
+
+	"go.undefinedlabs.com/scopeagent/rand"
 )
 
 var (
-	seededIDGen = rand.New(rand.NewSource(time.Now().UnixNano()))
-	// The golang rand generators are *not* intrinsically thread-safe.
-	seededIDLock sync.Mutex
+	randompool = rand.NewPool(time.Now().UnixNano(), uint64(max(16, runtime.NumCPU())))
 )
 
+// max returns the larger value among a and b
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func randomID() uint64 {
-	seededIDLock.Lock()
-	defer seededIDLock.Unlock()
-	return uint64(seededIDGen.Int63())
+	return uint64(randompool.Pick().Int63())
 }
 
 func randomID2() (uint64, uint64) {
-	seededIDLock.Lock()
-	defer seededIDLock.Unlock()
-	return uint64(seededIDGen.Int63()), uint64(seededIDGen.Int63())
+	n1, n2 := randompool.Pick().TwoInt63()
+	return uint64(n1), uint64(n2)
 }
