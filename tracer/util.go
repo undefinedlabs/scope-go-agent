635ead6fdca19e5d46adc291c3dc7bfca9e3ac6c
Daniel Rodriguez Hernandez <daniel.rguez.hdez.github@gmail.com>
2020-04-16 13:16:58 +0200

    Use crypto/rand to generate random seed (#223)

--- tracer/util.go
+++ tracer/util.go
@@ -1,16 +1,30 @@
 package tracer
 
 import (
+	cryptorand "crypto/rand"
+	"encoding/binary"
 	"math/rand"
 	"sync"
 	"time"
 )
 
 var (
-	seededIDGen = rand.New(rand.NewSource(time.Now().UnixNano()))
+	seededIDGen = rand.New(rand.NewSource(generateSeed()))
 	// The golang rand generators are *not* intrinsically thread-safe.
 	seededIDLock sync.Mutex
 )
+
+func generateSeed() int64 {
+	var b [8]byte
+	_, err := cryptorand.Read(b[:])
+	if err != nil {
+		// Cannot seed math/rand package with cryptographically secure random number generator
+		// Fallback to time.Now()
+		return time.Now().UnixNano()
+	}
+
+	return int64(binary.LittleEndian.Uint64(b[:]))
+}
 
 func randomID() uint64 {
 	seededIDLock.Lock()
